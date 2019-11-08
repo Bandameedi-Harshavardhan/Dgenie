219439e9f233b4f262a07d3e1bfc1137453197b1
Bandameedi-Harshavardhan <harsha1360088@gmail.com>
2019-11-08 18:02:37 +0530

    midterm evaluation part done

--- trydjango/pages/models.py
+++ trydjango/pages/models.py
@@ -7,3 +7,9 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Product(models.Model):
+	name		= models.TextField()
+	username	= models.TextField()
+	password	= models.TextField()
+	category	= models.TextField()
